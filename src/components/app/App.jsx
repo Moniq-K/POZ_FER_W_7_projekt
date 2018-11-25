fe11f17234f165d9d73bcd2137536aa311658963
Monika <monika.architekt@gmail.com>
2018-11-25 16:25:38 +0100

    build

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import styles from "./App.scss";
+import image from "../../assets/image.jpeg";
 
 export default class App extends React.Component {
     render(){
-        return <div>To jest App 1</div>
+        return <div className={styles.app}>To jest App 4<img src={image}/></div>
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,93 @@
 //Konfiguracja Webpack
 
-
 module.exports = {
+
     entry: "./src/index.js",
+
     output: {
+
         filename: "app.js",
+
         path: __dirname + "/docs"
+
     },
+
+    resolve: {
+
+        extensions: [".js", ".jsx"]
+
+    },
+    devServer: {
+        contentBase:__dirname+"/docs"
+    },
+
     module: {
+
         rules: [
+
             {
 
                 test: /.jsx?/,
+
                 loader: "babel-loader",
+
                 exclude: /node_modules/,
+
                 options: {
+
                     presets: ["@babel/preset-react"]
+
                 }
+
+            },
+
+            {
+
+                test: /.scss$/,
+
+                use: [
+
+                    {loader: "style-loader"},
+
+                    {
+
+                        loader: "css-loader",
+
+                        options: {
+
+                            modules: true,
+
+                            localIdentName: '[name]__[local]--[hash:base64:5]'
+
+                        }
+
+                    },
+
+                    {loader: "sass-loader"}
+
+                ]
+
+            },
+
+            {
+
+                test: /\.(png|jpg}gif|jpeg)$/i,
+
+                use: [
+
+                    {
+
+                        loader: "url-loader"
+
+                    }
+
+                ]
+
             }
+
         ]
+
     }
+
 }
+
